1dbf337c2b9a26dc88512c4a83a989490be9cf2c
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-05-17 19:45:09 +0100

    some more changes added

--- pet_shop.rb
+++ pet_shop.rb
@@ -26,9 +26,55 @@ def increase_pets_sold(pet_shop, number)
 end
 
 #7
-# def stock_count(pets)
-#     return pet_shop [:pets].length
+def stock_count(pet_shop)
+    return pet_shop [:pets].length
+end
+
+#8
+# def total_pets_by_breed(pets, breed)
+#     pets_by_breed = 0
+
+#     for pet in pets
+#         pets_by_breed += pets[:breed]
+#         breed[:breed] = 0
+#     end
+#     return pets_by_breed
 # end
+
+#9
+
+
+#10
+# def find_pet_by_name(pets, name)
+#     for pet in pets
+#         if pet[:name] == name
+#             return pet
+#         end
+#     end
+#     return nil
+# end
+
+#11
+
+#12
+
+def remove_pet_by_name(pets)
+    pets.delete_at("Arthur")
+end
+
+
+# def pets_by_breed(pet_shop)
+#    pets.count("British shorthair")
+# end
+
+#9
+# def pets_by_breed(pet_shop)
+#     pet_shop.count("Dalmation")
+# end
+
+#10
+
+
 
 #filter items from an array
 # Test test_all_pets_by_breed__found
