4e0af1e87f5ecb8b1e36597eccdf26256a1aa4d0
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-05-17 15:01:30 +0100

    Added functionality to add and subtract from total_cash

--- pet_shop.rb
+++ pet_shop.rb
@@ -8,8 +8,10 @@ def total_cash(pet_shop)
     return (pet_shop[:admin][:total_cash])
 end
 
-#test #3
-def add_or_remove_cash
-    @pet_shop(:admin :total_cash) << 10
+#test #3 4
+def add_or_remove_cash(pet_shop, number)
+    pet_shop[:admin][:total_cash] += number
+    return pet_shop[:admin][:total_cash]
 end
 
+#5
